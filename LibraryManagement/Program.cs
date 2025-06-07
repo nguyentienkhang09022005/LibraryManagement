205f774570f570d75c928633e2dc5b19866da3f0
Minh Khoa <tukhoa040505@gmail.com>
2025-06-07 16:41:59 +0700

    add

--- LibraryManagement/Program.cs
+++ LibraryManagement/Program.cs
@@ -163,6 +163,11 @@ builder.Services.AddFluentEmail("noreply@gmail.com", "no reply")
                     EnableSsl =  emailConfig.EnableSSL
                 });
 builder.Services.AddMemoryCache();
+
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
