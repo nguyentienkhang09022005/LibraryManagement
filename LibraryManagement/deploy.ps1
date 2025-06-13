7bb703f0a4c936181c49facec37f181ec4f32ad6
Minh Khoa <tukhoa040505@gmail.com>
2025-06-13 15:46:22 +0700

    add

--- LibraryManagement/deploy.ps1
+++ LibraryManagement/deploy.ps1
@@ -14,9 +14,21 @@ Write-Host "Deploying with env vars: $envVars"
 
 # Deploy bằng gcloud
 gcloud run deploy librarymanagement-api `
-  --image tukhoa040505/librarymanagement-api:latest `
+  --image tukhoa040505/librarymanagement-api:1.0.0 `
   --platform managed `
   --region asia-southeast1 `
   --allow-unauthenticated `
   --port 8080 `
   --set-env-vars $envVars
+
+  
+# Giữ lại 3 revision mới nhất, xóa phần còn lại
+$oldRevisions = gcloud run revisions list `
+  --service=librarymanagement-api `
+  --region=asia-southeast1 `
+  --sort-by="~metadata.creationTimestamp" `
+  --format="value(metadata.name)" | Select-Object -Skip 3
+
+foreach ($rev in $oldRevisions) {
+  gcloud run revisions delete $rev --region=asia-southeast1 --quiet
+}
